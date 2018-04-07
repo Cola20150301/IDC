7be1e9e35f95c363deb1f33998929c973fe2fe37
2116659665@qq.com <chen@gh@1010>
2018-04-07 10:11:11 +0800

    test

--- remote_server.py
+++ remote_server.py
@@ -5,12 +5,14 @@
 import tornado.ioloop
 import tornado.web
 import json
-
+import requests
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        print self.get_argument('id')
-        self.write({"message": "Hello, world"})
+        print self.get_argument('host_id')
+        res = requests.get(
+            'http://10.160.148.38/nagiosxi/api/v1/objects/hoststatus?apikey=uqqT9qkYWohKgd3h5ElfLgSGXhbrbfbIIEB7lbmhDG09SjpdW5rXkEDCb5jljt4i&pretty=1&host_id=4663')
+        self.write({"message": res.json()})
 
     def post(self):
         param = json.loads(self.request.body.decode('utf-8'))
